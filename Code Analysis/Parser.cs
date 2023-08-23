3f2bc5a0df6c3cea7caf1ed7f3c0cffc6e446377
dieghomj <diegomartj03@gmail.com>
2023-08-23 11:08:21 -0400

    refactor: Make precedence for parser data

--- Code Analysis/Parser.cs
+++ Code Analysis/Parser.cs
@@ -17,44 +17,46 @@ namespace HULK
         private SyntaxToken[] _tokens;
         private List<string> _diagnostics = new List<string>();
 
-        public Parser(string text){
+        public Parser(string text)
+        {
 
             //the constructor tokenizes the line and stores the tokens in the tokens array <_tokens>
 
             var tokens = new List<SyntaxToken>();
-            
+
             var lexer = new Lexer(text);
             SyntaxToken token;
-            
+
             do
             {
                 token = lexer.Lex();
 
-                if( token.Kind != SyntaxKind.WitheSpaceToken &&
+                if (token.Kind != SyntaxKind.WitheSpaceToken &&
                     token.Kind != SyntaxKind.BadToken)
                 {
                     tokens.Add(token);
                 }
             }
-            while( token.Kind != SyntaxKind.EndOfFileToken);
+            while (token.Kind != SyntaxKind.EndOfFileToken);
 
             _tokens = tokens.ToArray();
             _diagnostics.AddRange(lexer.Diagnostics);
-        } 
+        }
 
         public IEnumerable<string> Diagnostics => _diagnostics;
 
         private SyntaxToken Peek(int offset)
         {
             var index = _position + offset;
-            if(index >= _tokens.Length)
+            if (index >= _tokens.Length)
                 return _tokens[_tokens.Length];
-            return  _tokens[index];
+            return _tokens[index];
         }
 
         private SyntaxToken Current => Peek(0);
 
-        private SyntaxToken NextToken(){
+        private SyntaxToken NextToken()
+        {
             var current = Current;
             _position++;
             return current;
@@ -62,73 +64,72 @@ namespace HULK
 
         private SyntaxToken Match(SyntaxKind kind)
         {
-            if(Current.Kind == kind)return NextToken();
-            
+            if (Current.Kind == kind) return NextToken();
+
             _diagnostics.Add($"ERROR: Unexpected token <{Current.Kind}> expected <{kind}>");
             return new SyntaxToken(kind, Current.Position, null, null);
         }
 
         public SyntaxTree Parse()
         {
-            var expression =  ParseExpression();
+            var expression = ParseExpression();
             var endOfFileToken = Match(SyntaxKind.EndOfFileToken);
             return new SyntaxTree(_diagnostics, expression, endOfFileToken);
         }
 
-        private ExpressionSyntax ParseExpression(){
-            return ParseTerm();
-        }
-
-        private ExpressionSyntax ParseTerm()
-        {
-            var left = ParseFactor();
-
-            while (Current.Kind == SyntaxKind.PlusToken ||
-                    Current.Kind == SyntaxKind.MinusToken)
-            {
-                var operatorToken = NextToken();
-                var right = ParseFactor();
-                left = new BinaryExpressionSyntax(left, operatorToken, right);
-
-            }
-
-            return left;
-        }
-
-        private ExpressionSyntax ParseFactor()
+        private ExpressionSyntax ParseExpression(int parentPrecedence = 0)
         {
             var left = ParsePrimaryExpression();
 
-            while (Current.Kind == SyntaxKind.StarToken ||
-                    Current.Kind == SyntaxKind.DivToken)
+            while(true)
             {
-                var operatorToken = NextToken();
-                var right = ParsePrimaryExpression();
-                left = new BinaryExpressionSyntax(left, operatorToken, right);
+                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                if(precedence == 0 || precedence <= parentPrecedence)
+                    break;
 
+                var operatorToken = NextToken(); 
+                var right = ParseExpression(precedence);
+                left = new BinaryExpressionSyntax(left, operatorToken, right);
             }
 
             return left;
+        }
+
+        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
+        {
+            switch(kind)
+            {
+                case SyntaxKind.StarToken:
+                case SyntaxKind.DivToken:
+                    return 2;
+                case SyntaxKind.PlusToken:
+                case SyntaxKind.MinusToken:
+                    return 1;
+                default :
+                    return 0;
+            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression()
         {
 
-            if(Current.Kind == SyntaxKind.MinusToken){
-                
+            if (Current.Kind == SyntaxKind.MinusToken)
+            {
+
                 var zero = new SyntaxToken(SyntaxKind.NumberToken, -1, "0", 0);
                 var left = new LiteralExpressionSyntax(zero);
                 var operatorToken = NextToken();
                 var right = ParsePrimaryExpression();
 
-                return new BinaryExpressionSyntax(left,operatorToken, right);
+                return new BinaryExpressionSyntax(left, operatorToken, right);
             }
 
-            if(Current.Kind == SyntaxKind.OpenParenthisisToken){
+            if (Current.Kind == SyntaxKind.OpenParenthisisToken)
+            {
                 var left = NextToken();
                 var expression = ParseExpression();
                 var right = Match(SyntaxKind.CloseParenthisisToken);
-                return new ParenthisizedExpressionSyntax(left,expression,right);
+                return new ParenthisizedExpressionSyntax(left, expression, right);
             }
 
             var numberToken = Match(SyntaxKind.NumberToken);
