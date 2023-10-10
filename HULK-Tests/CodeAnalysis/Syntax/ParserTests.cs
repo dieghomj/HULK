63f6fea6086411bec40220f44e815ac67e9b203e
dieghomj <diegomartj03@gmail.com>
2023-10-10 15:10:40 -0400

    Added tests for binary operator precedence

--- HULK-Tests/CodeAnalysis/Syntax/ParserTests.cs
+++ HULK-Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -5,14 +5,6 @@ using Xunit;
 
 namespace HULK_Tests.CodeAnalysis.Syntax
 {
-    internal sealed class AssertingEnumerator
-    {
-        public ParserTests(Parameters)
-        {
-            
-        }
-    }
-
     public class ParserTests
     {
         [Theory]
@@ -21,20 +13,35 @@ namespace HULK_Tests.CodeAnalysis.Syntax
         {
             var op1Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op1);
             var op2Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op2);
-            
+
             var op1Text = SyntaxFacts.GetText(op1);
             var op2Text = SyntaxFacts.GetText(op2);
-            
-            var text = $"a {op1Text} b {op2Text} c";
 
-            if(op1Precedence > op2Precedence)
+            var text = $"a {op1Text} b {op2Text} c";
+            var expression = SyntaxTree.Parse(text).Root;
+
+
+            if (op1Precedence >= op2Precedence)
             {
                 //       op2
-                //      /   \
-                //    op1    c
+                //      /  \
+                //    op1   c
                 //   /  \
                 //  a    b
-                Assert.False(true);
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    e.AssertToken(op1,op1Text);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                    e.AssertToken(op2, op2Text);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "c");
+                }
+
             }
             else
             {
@@ -43,15 +50,32 @@ namespace HULK_Tests.CodeAnalysis.Syntax
                 //    a    op2
                 //        /  \
                 //       b    c
-                Assert.False(true);
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    e.AssertToken(op1, op1Text);
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                    e.AssertToken(op2, op2Text);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "c");
+                }
             }
         }
 
         private static IEnumerable<object[]> GetBinaryOperatorPairsData()
         {
-            foreach (var op1 in SyntaxFacts.GetUnaryOperatorKinds())
+            foreach (var op1 in SyntaxFacts.GetBinaryOperatorKinds())
+            {
                 foreach (var op2 in SyntaxFacts.GetBinaryOperatorKinds())
-                    yield return new object[] { op1, op2 }; 
+                {
+                    yield return new object[] { op1, op2 };
+                    // yield break;
+                }
+            }
         }
     }
 }
